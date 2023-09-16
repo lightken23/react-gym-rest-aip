b61133a25ae34a4ecce8901327103cb9b21d4864
lightken23 <90974114+VirenGohil@users.noreply.github.com>
2023-09-16 18:30:48 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions =  {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {  
+    'X-RapidAPI-Key': 'c631e19b30mshfb2240320e02f66p1d5cf0jsn1f7dd01a19de',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
